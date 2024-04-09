6a5c46dd7f1a9db34cc070419f7ff258083f7d99
rimworlds <ethan.capus@outlook.fr>
2024-04-09 14:01:06 +0200

    test ia

--- traning2.py
+++ traning2.py
@@ -2,7 +2,7 @@ from tflite_model_maker import object_detector
 from tflite_model_maker.object_detector import DataLoader
 
 # Chemin vers le dossier principal contenant les sous-dossiers d'images
-data_dir = 'chemin/vers/dataset'
+data_dir = 'Aircraft Carrier'
 
 # Créer un DataLoader à partir des images dans les sous-dossiers
 data_loader = DataLoader.from_folder(data_dir)
